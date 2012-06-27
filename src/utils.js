be2280e52e4ceb2764cce596b0d4d9d8284f649b
Tom Flanagan <knio@zkpq.ca>
2012-06-26 18:20:20 -0700

    clean up 2

--- src/utils.js
+++ src/utils.js
@@ -2,79 +2,59 @@
 
   var U = {
 
-    foreach: function(obj, func, context)
-    {
+    foreach: function(obj, func, context) {
       context = context || null;
-      if (obj instanceof Array)
-      {
-        for (var i = 0; i < obj.length; i++)
-        {
-          var r = func.call(context, obj[i], i);
-          if (r !== undefined)
-          {
-            return r;
-          }
+      var r;
+      if (obj instanceof Array) {
+        for (var i = 0; i < obj.length; i++) {
+          r = func.call(context, obj[i], i);
+          if (r !== undefined) { return r; }
         }
-      }
-      else
-      {
-        for (var k in obj)
-        {
-          if (obj.hasOwnProperty(k))
-          {
-            var r = func.call(context, obj[k], k);
-            if (r !== undefined)
-            {
-              return r;
-            }
+      } else {
+        for (var k in obj) {
+          if (!obj.hasOwnProperty(k)) { continue; }
+          r = func.call(context, obj[k], k);
+          if (r !== undefined) {
+            return r;
           }
         }
       }
     },
 
-    map: function(obj, func, context)
-    {
+    map: function(obj, func, context) {
       context = context || null;
       var ret;
       if (obj instanceof Array) { ret = []; }
       else                      { ret = {}; }
-      U.foreach(obj, function(v, k)
-      {
+      U.foreach(obj, function(v, k) {
         ret[k] = func.call(context, v, k);
       });
       return ret;
     },
 
-    filter: function(obj, func, context)
-    {
+    filter: function(obj, func, context) {
       context = context || null;
       var ret;
       if (obj instanceof Array) { ret = []; }
       else                      { ret = {}; }
-      U.foreach(obj, function(v, k)
-      {
-        if (func.call(context, v, k))
-        {
+      U.foreach(obj, function(v, k) {
+        if (func.call(context, v, k)) {
           ret[k] = v;
         }
       });
       return ret;
     },
 
-    remove: function(obj, func, context)
-    {
+    remove: function(obj, func, context) {
       context = context || null;
-      return U.filter(obj, function(v, k)
-      {
+      return U.filter(obj, function(v, k) {
         return !func.call(context, v, k);
       });
     },
 
-    mix: function(dst, src, overrride)
-    {
-      U.foreach(src, function(v, k)
-      {
-        if (!override && dst.hasOwnProperty(k)) { continue; }
+    mix: function(dst, src, override) {
+      U.foreach(src, function(v, k) {
+        if (!override && dst.hasOwnProperty(k)) { return; }
         dst[k] = v;
       });
       return dst;
